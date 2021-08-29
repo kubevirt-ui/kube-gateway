34b51cd4231c82d609895ef4155744c78b2fc598
yzamir <yzamir@localhost.localdomain>
2021-08-29 13:56:21 +0300

    remove null file

--- cmd/kube-gateway/utils.go
+++ cmd/kube-gateway/utils.go
@@ -17,9 +17,13 @@ import (
 	"github.com/golang/glog"
 	"golang.org/x/oauth2"
 	corev1 "k8s.io/api/core/v1"
-
-	"github.com/kubevirt-ui/kube-gateway/pkg/oauth"
 )
+
+// Endpoint holds the API server authorization URL.
+type Endpoint struct {
+	Auth  string `json:"authorization_endpoint"`
+	Token string `json:"token_endpoint"`
+}
 
 // PrintHelpMsg prints a help message and exit with success status
 func PrintHelpMsg() {
@@ -80,7 +84,7 @@ func GetJWTPuplicKey(bearer string, APITransport http.RoundTripper, APIServerURL
 
 // GetOAuthConf gets the oauth2 config object
 func GetOAuthConf(oauthServerAuthURL *string, oauthServerTokenURL *string, apiServer *string, gatewayBaseAddress *string, oauthServerClientID *string, oauthServerClientSecret *string, transport *http.Transport) *oauth2.Config {
-	var endpoint oauth.Endpoint
+	var endpoint Endpoint
 	var oauthConf *oauth2.Config
 	var err error
 
@@ -89,7 +93,7 @@ func GetOAuthConf(oauthServerAuthURL *string, oauthServerTokenURL *string, apiSe
 		endpoint.Token = *oauthServerTokenURL
 		endpoint.Auth = *oauthServerAuthURL
 	} else {
-		endpoint, err = oauth.GetServerEndpoint(*apiServer, transport)
+		endpoint, err = GetServerEndpoint(*apiServer, transport)
 		if err != nil {
 			LogErrorAndExit(err)
 		}
@@ -150,4 +154,22 @@ func GetTLSTranport(skipVerifyTLS *bool, apiServerCAFile *string) *http.Transpor
 	}
 
 	return transport
+}
+
+// GetServerEndpoint gets the API server well known oauth authorization endpoints.
+func GetServerEndpoint(serverURL string, transport *http.Transport) (Endpoint, error) {
+	var endpoint Endpoint
+	client := &http.Client{Transport: transport, Timeout: 30 * time.Second}
+
+	wellKnownURL := fmt.Sprintf("%s/.well-known/oauth-authorization-server", serverURL)
+	resp, err := client.Get(wellKnownURL)
+	if err != nil {
+		return endpoint, fmt.Errorf("fail to get well-known oauth-authorization-server: %+v", err)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&endpoint); err != nil {
+		return endpoint, fmt.Errorf("fail to get well known authorization endpoints: %+v", err)
+	}
+
+	return endpoint, nil
 }
